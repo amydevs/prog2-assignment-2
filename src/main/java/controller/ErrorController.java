ead64fda6e9058e5c7dab0da029683639b7d0501
Amy Y <ctrlalternativedelete@gmail.com>
2024-05-08 00:44:01 +1000

    errors stuff

--- src/main/java/controller/ErrorController.java
+++ src/main/java/controller/ErrorController.java
@@ -1,7 +1,21 @@
 package controller;
 
+import au.edu.uts.ap.javafx.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import model.InputException;
 
+public class ErrorController extends Controller<InputException>  {
+    @FXML
+    private Text errorMessageText;
 
-public class ErrorController  {
-    
+    @FXML
+    private void initialize() {
+        this.errorMessageText.setText(this.model.getMessage());
+    }
+
+    @FXML
+    private void close() {
+        this.stage.close();
+    }
 }
