386a8fddfa85f57198a569b16965a31b9c56c882
Amy Y <ctrlalternativedelete@gmail.com>
2024-05-08 22:21:02 +1000

    wip

--- src/main/java/controller/SeasonController.java
+++ src/main/java/controller/SeasonController.java
@@ -1,11 +1,36 @@
 package controller;
 
 import au.edu.uts.ap.javafx.ViewLoader;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import au.edu.uts.ap.javafx.Controller;
 import model.Season;
 
 
 public class SeasonController extends Controller<Season>  {
 
+    @FXML
+    private void roundMenu() {
+        try {
+            Stage stage = new Stage();
+            stage.setX(ViewLoader.X + 601);
+            stage.setY(ViewLoader.Y);
+            stage.getIcons().add(new Image("/view/nba.png"));
+            ViewLoader.showStage(this.model, "/view/SeasonRoundView.fxml", "Season Rounds", stage);
+        } catch (IOException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void close() {
+        this.stage.close();
+    }
 }
 
--- src/main/java/controller/TeamsRoundController.java
+++ src/main/java/controller/TeamsRoundController.java
@@ -1,8 +1,25 @@
 package controller;
 
+import au.edu.uts.ap.javafx.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import model.Season;
+import model.Team;
 
-public class TeamsRoundController  {
+public class TeamsRoundController extends Controller<Season>  {
+    @FXML
+    private ListView<Team> teamsListView;
 
+    public String getRoundString() {
+        return "Round: " + this.model.round();
+    }
+
+    @FXML
+    private void initialize() {
+        teamsListView.setItems(this.model.getCurrentTeams());
+        teamsListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    }
 }
 
 
