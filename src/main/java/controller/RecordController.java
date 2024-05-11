c202ef731915981bf04684a750f7870470fe36cd
Amy Y <ctrlalternativedelete@gmail.com>
2024-05-12 00:00:01 +1000

    done!

--- src/main/java/controller/RecordController.java
+++ src/main/java/controller/RecordController.java
@@ -1,10 +1,25 @@
 package controller;
 
+import au.edu.uts.ap.javafx.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import model.Season;
+import model.Record;
 
-public class RecordController {
+public class RecordController extends Controller<Season> {
+    @FXML
+    private TableView<Record> recordsTableView;
 
 
+    @FXML
+    private void initialize() {
+        this.recordsTableView.setItems(this.model.record());
+    }
     
+    @FXML
+    private void close() {
+        this.stage.close();
+    }
 }
 
 
--- src/main/java/controller/SeasonController.java
+++ src/main/java/controller/SeasonController.java
@@ -1,15 +1,14 @@
 package controller;
 
-import au.edu.uts.ap.javafx.ViewLoader;
-import javafx.fxml.FXML;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import au.edu.uts.ap.javafx.Controller;
+import au.edu.uts.ap.javafx.ViewLoader;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import model.InputException;
 import model.Season;
 
@@ -51,6 +50,19 @@ public class SeasonController extends Controller<Season>  {
             stage.setY(ViewLoader.Y);
             stage.getIcons().add(new Image("/view/nba.png"));
             ViewLoader.showStage(this.model, "/view/CurrentRoundTeams.fxml", "Tournament", stage);
+        } catch (IOException ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void recordMenu() {
+        try {
+            Stage stage = new Stage();
+            stage.setX(ViewLoader.X + 601);
+            stage.setY(ViewLoader.Y);
+            stage.getIcons().add(new Image("/view/nba.png"));
+            ViewLoader.showStage(this.model, "/view/RecordView.fxml", "Season Record", stage);
         } catch (IOException ex) {
             Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
         }
